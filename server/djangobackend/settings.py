ffecdf00eba8586cfc791bf7e5329ed97ed542d3
Marvel Lawson <lawsonmarvel@mail.com>
2022-01-23 18:44:44 +0000

    updated

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost","dealers-293.eu-de.cf.appdomain.cloud"]
+ALLOWED_HOSTS = ["localhost","dealers-293.eu-de.cf.appdomain.cloud","127.0.0.1"]
 
 
 # Application definition
